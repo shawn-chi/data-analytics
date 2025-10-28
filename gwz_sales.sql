b0b61cf2325551f3891e245304f29c9b86497be2
Shawn Chi <67786498+shawn-chi@users.noreply.github.com>
2025-10-28 14:16:11 +0100

    join_table

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,27 +1,22 @@
 -- Make gwz_sales_margin
-
-WITH gwz_sales_margin AS (
 SELECT *
 FROM `woven-invention-475010-a4.course14.gwz_sales` AS sales
 LEFT JOIN `woven-invention-475010-a4.course14.gwz_product` AS product
 USING (orders_id)
-)
-;
-gwz_orders_operational AS (
+
+
+WITH gwz_orders_operational AS (
 SELECT 
   *
 FROM `woven-invention-475010-a4.course14.gwz_orders` AS sales
 LEFT JOIN `woven-invention-475010-a4.course14.gwz_ship` AS ship
 USING (orders_id)
 )
-;
-
-
--- Cal operational_margin
+ 
 SELECT 
   date_date,
   ROUND(SUM(order_margin + shipping_fee - log_cost - ship_cost),2) AS ops_daily_margin 
-FROM `woven-invention-475010-a4.course14.gwz_orders_operational` 
+FROM gwz_orders_operational
 GROUP BY date_date
 ORDER BY date_date ASC
 ;
